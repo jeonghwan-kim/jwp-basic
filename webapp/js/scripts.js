fe8400dcb4044de2de33e530ee71644e4bdb5211
김정환 <woowahan@gimjeonghwan-ui-MacBook-Pro.local>
2017-04-24 19:56:26 +0900

    답변달기 ajax 작업

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -8,3 +8,41 @@ String.prototype.format = function() {
   });
 };
 
+(function($){
+  function getValueOf(el) {
+    return $(el).val();
+  }
+  function addAnswer (e) {
+    console.log(e);
+    e.preventDefault();
+
+    var formData = {
+      title: getValueOf($('#writer')[0]),
+      body: getValueOf($('#contents')[0])
+    }
+
+    var querystring  = $('.answerWrite form').serialize()
+
+    console.log(querystring);
+
+    $.ajax({
+      type: 'post',
+      url: '/api/qna/addanswer',
+      data: querystring,
+      error: onError,
+      success: onSuccess
+    });
+
+    function onError(e) {
+      console.log(e);
+    }
+
+    function onSuccess(r) {
+      console.log(r);
+    }
+  }
+
+
+  $('.answerWrite input[type=submit]').click(addAnswer);
+
+})(jQuery)
