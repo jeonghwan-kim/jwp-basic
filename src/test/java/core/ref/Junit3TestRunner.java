9569dc847f96c0d46c9338bee8beb6c81e09a27a
김정환 <woowahan@gimjeonghwan-ui-MacBook-Pro.local>
2017-04-27 20:57:08 +0900

    [요구사항 2] test로 시작하는 메소드 실행

--- src/test/java/core/ref/Junit3TestRunner.java
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -1,11 +1,24 @@
 package core.ref;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
 
 public class Junit3TestRunner {
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Junit3Test junit3Test = clazz.newInstance();
 
+        for (Method method : clazz.getDeclaredMethods()) {
+            String name = method.getName();
+            if (name.startsWith("test")) {
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
