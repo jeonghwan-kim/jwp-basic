338576e91153cf42d0005ddd7df42a2403a58fa0
김정환 <woowahan@gimjeonghwan-ui-MacBook-Pro.local>
2017-04-27 21:13:38 +0900

    [요구사항 3] @Test 애노테이션 메소드 실행

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,11 +1,17 @@
 package core.ref;
 
 import org.junit.Test;
+import java.lang.reflect.Method;
 
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(clazz.newInstance());
+            }
+        }
     }
 }
